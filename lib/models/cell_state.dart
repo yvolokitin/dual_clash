8c907784dfd15f81298a850b2dce825daeef41cb
Yura Volokitin <yuri.volokitin@gmail.com>
2025-12-19 11:06:53 +0100

    Revert "initial commit"
    This reverts commit c9f6b7d16ddc13537c4dbc5ab469a2209d827cae.

--- lib/models/cell_state.dart
+++
@@ -1,9 +0,0 @@
-
-enum CellState { empty, red, blue, neutral }
-
-extension CellStateX on CellState {
-  bool get isEmpty => this == CellState.empty;
-  bool get isRed => this == CellState.red;
-  bool get isBlue => this == CellState.blue;
-  bool get isNeutral => this == CellState.neutral;
-}
