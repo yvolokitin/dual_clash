7f7f52db94cfb9b8fb7eb62331c965914a476f2e
Yura Volokitin <yuri.volokitin@gmail.com>
2026-01-05 05:45:10 +0000

    Use gold glow for challenge leader

--- lib/ui/widgets/game_page_score_row.dart
+++ lib/ui/widgets/game_page_score_row.dart
@@ -54,9 +54,10 @@ class GamePageScoreRow extends StatelessWidget {
       decoration: BoxDecoration(
         boxShadow: [
           BoxShadow(
-            color: Colors.black.withOpacity(0.35),
-            blurRadius: 4,
-            offset: const Offset(0, 3),
+            color: const Color(0xFFFFD166).withOpacity(0.9),
+            blurRadius: 10,
+            spreadRadius: 1,
+            offset: const Offset(0, 0),
           ),
         ],
       ),
