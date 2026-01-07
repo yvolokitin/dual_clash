d8358ca286b1753b69c283d9da355e21d99d2491
Yura Volokitin <yuri.volokitin@gmail.com>
2026-01-07 12:54:14 +0100

    Use settings title for language label

--- lib/ui/pages/settings_page.dart
+++ lib/ui/pages/settings_page.dart
@@ -184,7 +184,7 @@ class _SettingsDialogState extends State<SettingsDialog> {
                         crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
                           // Language selector
-                          _label(l10n.languageTitle),
+                          _label(l10n.settingsTitle),
                           Wrap(
                             spacing: 8,
                             runSpacing: 8,
