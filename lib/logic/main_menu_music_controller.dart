dd1e9c4183f4bdf91d52d1ac3672ebb9e5982435
Yura Volokitin <yuri.volokitin@gmail.com>
2026-01-15 10:13:41 +0100

    Fix menu music on Windows and Android

--- lib/logic/main_menu_music_controller.dart
+++ lib/logic/main_menu_music_controller.dart
@@ -1,3 +1,4 @@
+import 'package:audio_session/audio_session.dart';
 import 'package:flutter/foundation.dart';
 import 'package:just_audio/just_audio.dart';
 
@@ -11,6 +12,7 @@ class MainMenuMusicController {
   bool _enabled = true;
   bool _mainMenuVisible = false;
   bool _menuReady = false;
+  bool _sessionConfigured = false;
 
   Future<void> setEnabled(bool enabled) async {
     _enabled = enabled;
@@ -37,6 +39,7 @@ class MainMenuMusicController {
 
   Future<void> _play() async {
     try {
+      await _ensureAudioSession();
       if (!_isLoaded) {
         await _player.setAsset('assets/m4a/main_page.m4a');
         await _player.setLoopMode(LoopMode.one);
@@ -47,6 +50,17 @@ class MainMenuMusicController {
       }
     } catch (error) {
       debugPrint('MainMenuMusicController: failed to play music: $error');
+    }
+  }
+
+  Future<void> _ensureAudioSession() async {
+    if (_sessionConfigured) return;
+    try {
+      final session = await AudioSession.instance;
+      await session.configure(AudioSessionConfiguration.music());
+      _sessionConfigured = true;
+    } catch (error) {
+      debugPrint('MainMenuMusicController: audio session error: $error');
     }
   }
 
